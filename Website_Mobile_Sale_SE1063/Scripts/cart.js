a1a8c0970b4529ce159af379119c44c519baa0fb
khang-tuong <jakelong1232007@gmail.com>
2017-03-07 10:38:13 +0700

    Update project để login = google

---
+++ Website_Mobile_Sale_SE1063/Scripts/cart.js
@@ -0,0 +1,49 @@
+﻿var cartDetail = {
+    PhoneId,
+    Quantity,
+    Price
+}
+
+var cart = new Array();
+
+
+function Add(PhoneId, Quantity, Price) {
+    var c = new cartDetail(PhoneId, Quantity, Price);
+    cart.push(c);
+}
+
+/**
+* <b>Quantity</b>: number of increment
+*/
+function Update(PhoneId, Quantity) {
+    for (var i = 0; i < cart.length; i++) {
+        var c = cart[i];
+        if (c.PhoneId == PhoneId) {
+            c.Price = Price / c.Quantity * (c.Quantity + Quantity);
+            c.Quantity += Quantity;
+        }
+    }
+}
+
+function Remove(PhoneId, Quantity) {
+    for (var i = 0; i < cart.length; i++) {
+        var c = cart[i];
+        if (c.PhoneId == PhoneId) {
+            c.Price = Price / c.Quantity * (c.Quantity - Quantity);
+            c.Quantity -= Quantity;
+        }
+    }
+}
+
+function RemovePhone(PhoneId) {
+    for (var i = 0; i < cart.length; i++) {
+        var c = cart[i];
+        if (c.PhoneId == PhoneId) {
+            cart.splice(i, 1);
+        }
+    }
+}
+
+function RemoveAll() {
+    cart = new Array();
+}
