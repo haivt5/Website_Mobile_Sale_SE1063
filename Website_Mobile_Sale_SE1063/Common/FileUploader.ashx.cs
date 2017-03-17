08ab603c42634a2f4686259b5eed9c782c060a7d
haivt5 <vuthanhhai1995@gmail.com>
2017-03-18 05:07:30 +0700

    add new phone (fail)
    Khang ới, fix-bug giùm anh chứ anh bế tắc quá :'(
    field RAM cứ null hoài :'(((

---
+++ Website_Mobile_Sale_SE1063/Common/FileUploader.ashx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Common
+{
+    /// <summary>
+    /// Summary description for FileUploader
+    /// </summary>
+    public class FileUploader : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            try
+            {
+                string dirFullPath = HttpContext.Current.Server.MapPath("/Content/images/add_new_phone/");
+                string[] files;
+                int numFiles;
+                files = System.IO.Directory.GetFiles(dirFullPath);
+                numFiles = files.Length;
+                numFiles = numFiles + 1;
+                string str_image = "";
+
+                foreach (string s in context.Request.Files)
+                {
+                    HttpPostedFile file = context.Request.Files[s];
+                    string fileName = file.FileName;
+                    string fileExtension = file.ContentType;
+
+                    if (!string.IsNullOrEmpty(fileName))
+                    {
+                        fileExtension = Path.GetExtension(fileName);
+                        str_image = fileName + fileExtension;
+                        string pathToSave_100 = HttpContext.Current.Server.MapPath("/Content/images/add_new_phone/") + str_image;
+                        file.SaveAs(pathToSave_100);
+                    }
+                }
+                //  database record update logic here  ()
+
+                context.Response.Write(str_image);
+            }
+            catch (Exception err)
+            {
+
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
